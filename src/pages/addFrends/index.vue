7acf552b3f8a902785c666b42c2616bb8765558d
bx <996889692@qq.com>
2019-05-21 16:26:38 +0800

    222

---
+++ src/pages/addFrends/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+      <div class="head bg_fff ">
+        <!--头像部分-->
+          <div class="flex avatarhead">
+              <img src="/static/images/of/ava.png" alt="" class="avatar">
+              <div class="flex1">
+                  <p>哈哈哈哈</p>
+                  <p class="font_four">女 27岁 深圳</p>
+              </div>
+          </div>
+          <!--填写验证信息-->
+          <div class="vertical flex flexColumn">
+              <p class="font_four">填写验证信息</p>
+              <input type="text" v-model="verMsg" class="flex1 yanz">
+          </div>
+      </div>
+      <div class="setclass">设置备注与分组</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      verMsg:'我是',
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
