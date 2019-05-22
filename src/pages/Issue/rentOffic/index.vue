042dfd5d57511ba75d405e7b83fe3930b6f1e547
liling <907247899@qq.com>
2019-05-22 14:52:04 +0800

    666

---
+++ src/pages/Issue/rentOffic/index.vue
@@ -0,0 +1,417 @@
+<template>
+  <div class="pageContent">
+    <div class="FormBox">
+      <img class="topbgimg" src="/static/images/icons/bg1.jpg">
+      <!-- 短租房使用背景bg2.jpg -->
+      <div class="FormCon" style="margin-top:-260rpx;">
+        <div class="formTitle">
+          <div class="titletxt">寻找拼租表单</div>
+          <p class="subtitle">(办公室/商铺/工厂/培训机构/其他)</p>
+        </div>
+        <div class="form-cells">
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">标题</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请填写适合标题"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-bottom">
+              <div class="form-cells-hd">公司名称</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请输入公司名称"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item" style="padding-right:0">
+            <div class="form-cells-item">
+              <div class="form-cell-bd">深圳木棉科技有限公司</div>
+            </div>
+            <div class="form-cells-item">
+              <div class="form-cell-bd">深圳大漠时代文化传媒集团有限公司</div>
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">公司类别</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">意向拼租行业</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">计划租贷日期</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">意向区域</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">所需面积</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">价格区间</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">是否装修</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">卡位要求</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">公司简介</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请填写公司详细简介"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">公司理念</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请填写公司理念"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">办公室描述</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="描述一下办公室环境能够更加吸引优质候选人哦"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-hd">办公司照片上传</div>
+            <div class="form-cell-bd form-cell-img">
+              <div class="upbtn">
+                <span class="close">×</span>
+                <img class="upimg" src="/static/images/of/index_a1.jpg">
+              </div>
+              <div class="upbtn">
+                <img class="upimg" src="/static/images/icons/upload_1.png">
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">可否注册</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">可否挂牌</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">业务分包</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">资质使用</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">股份合作</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">实缴资本</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">公司地址</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="办公大楼名称 例：京基大厦"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div>
+              <div class="form-cells-hd">门牌号</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="门牌号/楼号等 例：3楼416室"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-hd">拼租设施/设备</div>
+            <div class="form-cell-bd">
+              <div class="btns-group">
+                <div class="btn cur">网络/无线</div>
+                <div class="btn cur">打印机</div>
+                <div class="btn cur">会议室</div>
+                <div class="btn">前台</div>
+                <div class="btn">独立卡座</div>
+                <div class="btn">自定义+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 以下是组建公司显示 -->
+      <div class="formTitle">
+        <div class="titletxt c_333">想看看什么样的人</div>
+        <p class="subtitle">将根据您的要求推荐优质合伙人</p>
+      </div>
+      <div class="FormCon mb10">
+        <div class="form-cells">
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">职位名称</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请输入职位名称"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">经验要求</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">最低学历</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">合伙待遇</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+            <div class="item2-column">
+              <div class="form-cells-hd">全职/兼职</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item form-cells-item2">
+            <div class="item2-column">
+              <div class="form-cells-hd">工作时间</div>
+              <input
+                class="ipt"
+                type="text"
+                disabled
+                placeholder="请选择"
+                placeholder-style="color:#b5b5b5;"
+              >
+            </div>
+          </div>
+          <div class="form-cells-item">
+            <div class="form-cells-navigate navigate-right">
+              <div class="form-cells-hd">职位描述</div>
+              <div class="form-cell-bd">
+                <input
+                  class="ipt"
+                  type="text"
+                  placeholder="请填写职位详细信息"
+                  placeholder-style="color:#b5b5b5;"
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="AddBtn_info">
+        <img class="icon" src="/static/images/icons/add_icon.png" alt="">
+        <div class="tips">添加新的职位信息</div>
+      </div>
+      <div class="ftbox">
+        <div class="btn">确认发布</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/style/style_fb.scss";
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {
+    this.setBarTitle();
+  },
+  components: {},
+  methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "发布"
+      });
+    }
+  },
+
+  created() {}
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Issue/submenu/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="pageContent bg_fff" style="min-height:100vh">
+    <div class="topTips">
+      <div class="Tipsbox">
+        <div class="icon">
+          <img src="/static/images/icons/tips_icon.png">
+        </div>
+        <div class="txt">温馨提示：请正确选择发布信息的分类，分类错误会导致信息违规下架</div>
+      </div>
+    </div>
+    <div class="navBox">
+      <ul class="navList li_25 center navList2">
+        <li>
+          <div class="outside" @click="gotoFrom">
+            <div class="icon-img">
+              <img src="/static/images/icons/bangongshi_2.jpg" alt>
+            </div>
+            <p class="title">办公室</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/kawei.jpg" alt>
+            </div>
+            <p class="title">卡位</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/huiyishi.jpg" alt>
+            </div>
+            <p class="title">会议室</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/peixun.jpg" alt>
+            </div>
+            <p class="title">培训机构</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/duanzu.jpg" alt>
+            </div>
+            <p class="title">短租</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/shangpu.jpg" alt>
+            </div>
+            <p class="title">商铺</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/gongchang.jpg" alt>
+            </div>
+            <p class="title">工厂</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/shebei.jpg" alt>
+            </div>
+            <p class="title">设备</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/shoumaiji.jpg" alt>
+            </div>
+            <p class="title">自动售卖机</p>
+          </div>
+        </li>
+        <li>
+          <div class="outside">
+            <div class="icon-img">
+              <img src="/static/images/icons/qita.jpg" alt>
+            </div>
+            <p class="title">其他</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {
+    this.setBarTitle();
+  },
+  components: {},
+  methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "发布"
+      });
+    },
+    gotoFrom(){
+      wx.navigateTo({
+        url: '/pages/Issue/rentOffic/main'
+      })
+    }
+  },
+
+  created() {}
+};
+</script>
+<style lang="scss" scoped>
+.topTips {
+  background: #fcdec2;
+  padding: 40rpx;
+  .Tipsbox {
+    .icon {
+      float: left;
+      height: 80rpx;
+      width: 80rpx;
+      margin-right: 20rpx;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .txt {
+      line-height: 40rpx;
+      color: #666
+    }
+  }
+}
+.navList {
+  font-size: 24rpx;
+  color: #999;
+  .outside {
+    color: #999;
+  }
+  &.li_25 .icon-img,
+  &.li_25 .icon-img img{
+    width: 100rpx;
+    height: 100rpx;
+  }
+}
+
+</style>
