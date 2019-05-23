dedb45eb8d5f6022d48ff16758f7412bc7aa378f
ldx1992 <928150957@qq.com>
2019-05-23 17:07:16 +0800

    做积分

---
+++ src/pages/member/memberManage/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pageContent">
+    
+  </div>
+</template>
+<script>
+export default {
+  onShow() {
+    this.setBarTitle();
+  },
+  data() {
+    return {
+      type: ""
+    };
+  },
+  methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "会员管理"
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
