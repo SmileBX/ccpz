fde6da90d656cde9feca572b5537ffc595d6ad91
ldx1992 <928150957@qq.com>
2019-05-27 13:51:49 +0800

    做登录的静态页面

---
+++ src/pages/codeLogin/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="pageContent bg_fff">
+    <div class="LoginBox">
+      <img src="/static/images/icons/logo.jpg" class="logo" alt>
+      <div class="from">
+        <div class="line flex">
+          <input type="text"  class=" weui-input flex1" placeholder="请输入手机号码">
+          <span class="btnCode">获取动态码</span>
+        </div>
+        <div class="line flex">
+          <input type="text" class="weui-input flex1" placeholder="动态码">
+        </div>
+      </div>
+      <div class="ftBtn center" style="padding-top:60rpx;">
+        <div class="inner">
+          <div class="btns">
+            <div class="btn bg_ef8120 color_fff">登录</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  onLoad() {
+    this.setBarTitle();
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "验证码登录"
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.pageContent {
+  height: 100vh;
+  overflow: hidden;
+}
+.logo {
+  width: 466rpx;
+  height: 100rpx;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 80rpx;
+}
+.LoginBox{
+  border-top:1px solid #f2f2f2;
+  padding:206rpx 70rpx 70rpx; 
+}
+.from .line{
+  border-bottom: 1px solid #f2f2f2;
+  padding-top:12rpx;
+  padding-bottom: 12rpx;
+  margin-top:50rpx;
+}
+.bg_ef8120{
+  background: #ef8120;
+}
+.btnCode{
+  color:#666;
+}
+</style>
---
+++ src/pages/loginWay/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="pageContent bg_fff">
+    <div class="LoginBox">
+      <img src="/static/images/icons/loginPic.jpg" style="width:100%;" mode="widthFix" class="loginPic" alt="">
+      <div class="ftBtn">
+        <div class="inner">
+          <div class="btns center">
+            <div class="btn bg_3daf35">微信授权登陆</div>
+            <div class="btn bg_ef8120">成成账号登陆</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  onLoad() {
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+    
+  }
+};
+</script>
+<style lang="scss" scoped>
+.pageContent{
+  height: 100vh;
+  overflow: hidden;
+}
+.LoginBox{
+  width: 472rpx;
+  margin:0 auto;
+  padding:100rpx 0;
+}
+.bg_3daf35{
+  background: #3daf35;
+
+}
+.bg_ef8120{
+  background: #ef8120;
+}
+.ftBtn{
+  height: auto;
+  padding-top:30rpx;
+}
+.ftBtn .btn{
+  color:#fff;
+  border-radius: 16rpx;
+  margin-top:30rpx;
+}
+</style>
