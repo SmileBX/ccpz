fd5bcbfdb69fd54bd55ac4ebf1e336cd2dc14514
ldx1992 <928150957@qq.com>
2019-06-05 10:53:23 +0800

    做企业认证

--- src/pages/VerticalStatus/index.vue
+++ src/pages/VerticalStatus/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="page pageVertical">
-    <div class="top">
-      <div class="icon">
-        <img src="" alt="">
+    <div class="top center">
+      <div class="icon flex flexAlignCenter justifyContentCenter">
+        <img src="/static/images/icons/verticalStatus2.jpg" alt>
       </div>
-      <p class="status">审核中</p>
-      <img src="" mode="widthFix" class="VerticalBg" alt="">
+      <p class="status" style="margin-bottom:40rpx;">审核中</p>
+      <img src="/static/images/icons/verticalStatusBg.jpg" mode="widthFix" class="VerticalBg" alt>
     </div>
     <div class="msgBox">
       <h2 class="statusTips">提交成功，请等待管理员审核</h2>
@@ -18,7 +18,9 @@ import { post, toLogin, getCurrentPageUrlWithArgs } from "@/utils";
 export default {
   data() {
     return {
-      verticalType: 1, //1:个人认证；2：企业认证
+      type: 1, //1:发布审核；2：认证审核
+      verticalType:"",   //认证审核中是个人认证还是企业认证；1:个人认证；2：企业认证
+      id:"",  //需要审核的id
       userId: "",
       token: "",
       curPage: ""
@@ -28,6 +30,15 @@ export default {
     this.userId = wx.getStorageSync("userId");
     this.token = wx.getStorageSync("token");
     this.curPage = getCurrentPageUrlWithArgs();
+    if(this.$root.$mp.query.type && this.$root.$mp.query.type !=="undefined"){
+      this.type = this.$root.$mp.query.type;
+    }
+    if(this.$root.$mp.query.id && this.$root.$mp.query.id !=="undefined"){
+      this.id = this.$root.$mp.query.id;
+    }
+    if(this.$root.$mp.query.verticalType && this.$root.$mp.query.verticalType !=="undefined"){
+      this.verticalType = this.$root.$mp.query.verticalType;
+    }
   },
   methods: {
     setBarTitle() {
@@ -39,10 +50,45 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.top .icon{
-  width: 200rpx;
+.pageVertical{
+  height: 100vh;
+  overflow: hidden;
+  background: #fff;
 }
-.VerticalBg{
+.top {
+  color:#fff;
+  background: #ff952e;
+  padding-top:160rpx;
+  // padding-bottom: 60rpx;
+  .icon {
+    width: 194rpx;
+    height: 194rpx;
+    background: #fff;
+    border-radius: 50%;
+    margin:0 auto 20rpx;
+    img {
+      width: 94rpx;
+      height: 94rpx;
+    }
+  }
+  .status {
+    font-size: 44.62rpx;
+  }
+}
+.msgBox{
+  padding:120rpx;
+  color:#666;
+  margin-top:60rpx;
+  line-height: 1.4;
+  .statusTips{
+    margin-bottom: 18rpx;
+    font-size: 36rpx;
+    color:#333;
+    font-weight: 600;
+  }
+  text-align: center;
+}
+.VerticalBg {
   width: 100%;
   display: block;
 }
---
+++ src/pages/mine2/verticalCompany/index.vue
@@ -0,0 +1,382 @@
+<template>
+  <div class="pageContent pageVertical">
+    <div class="weui-cells">
+      <div class="weui-cell">
+        <div class="weui-cell_bd">
+          <p class="txt">企业名称</p>
+        </div>
+        <input type="text" v-model="name" placeholder="请输入企业名称">
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell_bd">
+          <p class="txt">企业注册号</p>
+        </div>
+        <input type="text" v-model="regNum" placeholder="请输入企业注册号">
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell_bd">
+          <p class="txt">法人姓名</p>
+        </div>
+        <input type="text" v-model="legalPerson" placeholder="请输入法人姓名">
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell_bd">
+          <p class="txt">法人身份证号</p>
+        </div>
+        <input type="text" v-model="idCard" placeholder="请输入法人身份证号">
+      </div>
+    </div>
+    <!--上传身份证-->
+    <div class="uploadBox bg_fff mt10">
+      <div>
+        <p class="font30 fontBold company_pic_title" style="padding:30rpx 30rpx 0;margin-bottom:0;">身份证照</p>
+        <div class="idendityPic flex justifyContentBetween">
+          <div class="imgBox">
+            <img
+              @click="upLoadImg(1)"
+              v-if="idCardPositive=='' "
+              src="/static/images/icons/iden-top.jpg"
+              alt
+              class="inendity_pic"
+            >
+            <img v-else :src="idCardPositive" alt class="inendity_pic">
+            <img
+              v-if="idCardPositive"
+              src="/static/images/icons/cancle.png"
+              @click="delImg(1)"
+              class="icon-del"
+              alt
+            >
+          </div>
+          <div class="imgBox">
+            <img
+              @click="upLoadImg(2)"
+              v-if="idCardNegative=='' "
+              src="/static/images/icons/iden-back.jpg"
+              alt
+              class="inendity_pic"
+            >
+            <img v-else :src="idCardNegative" alt class="inendity_pic">
+            <img
+              v-if="idCardNegative"
+              src="/static/images/icons/cancle.png"
+              @click="delImg(2)"
+              class="icon-del"
+              alt
+            >
+          </div>
+        </div>
+      </div>
+      <!--企业-->
+      <div style="padding:0 30rpx">
+        <!--企业上传营业执照-->
+        <div class="companyPic">
+          <p class="font30 fontBold company_pic_title">营业执照</p>
+          <div class="imgBox">
+            <img
+              @click="upLoadImg(3)"
+              v-if="businessLicense == '' "
+              src="/static/images/icons/card_com.jpg"
+              alt
+              class="company_pic"
+            >
+            <img v-else :src="businessLicense" alt class="company_pic">
+            <img
+              v-if="businessLicense"
+              src="/static/images/icons/cancle.png"
+              @click="delImg(3)"
+              class="icon-del"
+              alt
+            >
+          </div>
+        </div>
+        <!--企业上传其他照片-->
+        <div class="otherPic">
+          <p class="font30 fontBold company_pic_title">其他资质</p>
+          <div class="flex">
+            <div class="imgBox">
+              <img
+                @click="upLoadImg(4)"
+                v-if="!otherSeniority"
+                src="/static/images/icons/uppic.jpg"
+                alt
+                class="up_pic"
+              >
+              <img v-else :src="otherSeniority" alt class="up_pic">
+              <img
+                v-if="otherSeniority"
+                src="/static/images/icons/cancle.png"
+                @click="delImg(4)"
+                class="icon-del"
+                alt
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--企业end-->
+    </div>
+    <div class="ftBtn center">
+      <div class="inner">
+        <div class="btns">
+          <div class="btn color_fff bg_ff952e" @click="btnSubmit">下一步</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { post, toLogin, getCurrentPageUrlWithArgs,trim } from "@/utils";
+import { pathToBase64 } from "@/utils/image-tools";
+export default {
+  onLoad() {
+    this.setBarTitle();
+  },
+  onShow() {
+    this.userId = wx.getStorageSync("userId");
+    this.token = wx.getStorageSync("token");
+    this.curPage = getCurrentPageUrlWithArgs();
+  },
+  data() {
+    return {
+      userId: "",
+      token: "",
+      curPage: "",
+      name: "", //企业名称
+      regNum: "", //企业注册号,
+      legalPerson: "", //法人
+      idCard: "", //法人身份证号
+      idCardPositive: "", //身份证正面
+      idCardNegative: "", //身份证反面
+      businessLicense: "", //营业执照
+      otherSeniority: "" //其他资质
+    };
+  },
+  methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "认证新企业"
+      });
+    },
+    valOther() {
+      //认证的校验
+      if (trim(this.name) == "") {
+        wx.showToast({
+          title: "请输入企业名称!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.regNum) == "") {
+        wx.showToast({
+          title: "请输入企业注册号!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.legalPerson) == "") {
+        wx.showToast({
+          title: "请输入法人姓名!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.idCard) == "") {
+        wx.showToast({
+          title: "请输入法人身份证号!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (!/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(this.idCard)) {
+        wx.showToast({
+          title: "请输入正确的身份证号格式!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.idCardPositive) == "") {
+        wx.showToast({
+          title: "请上传身份证正面照!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.idCardNegative) == "") {
+        wx.showToast({
+          title: "请上传身份证反面照!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      if (trim(this.businessLicense) == "") {
+        wx.showToast({
+          title: "请上传营业执照!",
+          icon: "none",
+          duration: 1500
+        });
+        return false;
+      }
+      return true;
+    },
+    upLoadImg(index) {
+      //上传图片
+      let _this = this;
+      wx.chooseImage({
+        //进入这里面的时候this发生了改变
+        count: 1,
+        sizeType: ["compressed"],
+        sourceType: ["album", "camera"],
+        success(res) {
+          // tempFilePath可以作为img标签的src属性显示图片
+          let imgPathArr = res.tempFilePaths[0];
+          if (index == 1) {
+            _this.idCardPositive = imgPathArr;
+          }
+          if (index == 2) {
+            _this.idCardNegative = imgPathArr;
+          }
+          if (index == 3) {
+            _this.businessLicense = imgPathArr;
+          }
+          if (index == 4) {
+            _this.otherSeniority = imgPathArr;
+          }
+        }
+      });
+    },
+    delImg(index) {
+      if (index == 1) {
+        this.idCardPositive = "";
+      }
+      if (index == 2) {
+        this.idCardNegative = "";
+      }
+      if (index == 3) {
+        this.businessLicense = "";
+      }
+      if (index == 4) {
+        this.otherSeniority = "";
+      }
+    },
+    async base64Img(path) {
+      const base64Arr = await pathToBase64(path);
+      return base64Arr;
+    },
+    async btnSubmit() {
+      //提交下一步
+      if (this.valOther()) {
+        console.log("竟来了");
+        let idcardPositive = await this.base64Img(this.idCardPositive);
+        let idcardNegative = await this.base64Img(this.idCardNegative);
+        let businessLicense = await this.base64Img(this.businessLicense);
+        let otherSeniority ="";
+        if(this.otherSeniority){
+          otherSeniority = await this.base64Img(this.otherSeniority);
+        }
+        this.UserBusinessAuth(idcardPositive,idcardNegative,businessLicense,otherSeniority);
+      }
+    },
+    UserBusinessAuth(
+      idcardPositive,
+      idcardNegative,
+      businessLicense,
+      otherSeniority
+    ) {
+      let that = this;
+      post(
+        "User/UserBusinessAuth",
+        {
+          UserId: that.userId,
+          Token: that.token,
+          Company: {
+            Name: that.name,
+            RegNum: that.regNum,
+            LegalPerson: that.legalPerson,
+            Idcard: that.idCard,
+            IdcardPositive: idcardPositive,
+            IdcardNegative: idcardNegative,
+            BusinessLicense: businessLicense,
+            OtherSeniority: otherSeniority
+          }
+        },
+        that.curPage
+      ).then(res => {
+        if (res.code === 0) {
+          wx.showToast({
+            title: "提交资料成功",
+            icon: "none",
+            duration: 1500
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+<style lang='scss' scoped>
+.weui-cells .weui-cell::before {
+  display: block;
+}
+.btn_box {
+  .btnSub {
+    width: 630rpx;
+  }
+}
+.pageVertical .weui-cell {
+  font-size: 30rpx;
+}
+.pageVertical .companyPic .company_pic {
+  width: 690rpx;
+}
+.pageVertical .idendityPic .inendity_pic{
+  width:320rpx;
+}
+.pageVertical {
+  .card_bd {
+    background: #fff !important;
+  }
+  .avatarbox {
+    margin: 0;
+    width: 120rpx;
+    height: 120rpx;
+    position: absolute;
+    margin: 0;
+    top: -60rpx;
+    left: 50%;
+    margin-left: -40rpx;
+  }
+}
+.pageVertical .card_bd {
+  border-radius: 16rpx;
+  padding-top: 60rpx;
+  box-sizing: border-box;
+}
+.pageVertical .filterMenu {
+  margin-top: 0;
+}
+.icon-del {
+  width: 36rpx;
+  height: 36rpx;
+  position: absolute;
+  top: -18rpx;
+  right: -18rpx;
+  z-index: 3;
+}
+.imgBox {
+  position: relative;
+}
+.ftBtn {
+  padding: 30rpx;
+}
+.ftBtn .btn{
+  border-radius: 16rpx;
+}
+</style>
