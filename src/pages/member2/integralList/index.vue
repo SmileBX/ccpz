c040a4866e3d4034d1ba3a38213bc6caa41395a3
bx <996889692@qq.com>
2019-06-18 13:52:28 +0800

    111111111111

--- src/pages/member2/integralList/index.vue
+++ src/pages/member2/integralList/index.vue
@@ -2,31 +2,33 @@
   <div class="pageContent">
     <div class="filterMenu bg_fff" style="padding:0;">
       <ul class="menu bbLi__menu bbNo__menu li_33 flex center">
-        <li class="active">
+        <li :class="{'active':type===0}" @click="shiftMenu(0)">
           <div class="item">全部</div>
         </li>
-        <li>
+        <li :class="{'active':type===1}" @click="shiftMenu(1)">
           <div class="item">获得</div>
         </li>
-        <li>
+        <li :class="{'active':type===2}" @click="shiftMenu(2)">
           <div class="item">消耗</div>
         </li>
       </ul>
     </div>
     <scroll-view class="filterContent" scroll-y="true">
-      <ul class="detailList">
-        <li>
+      <ul class="detailList" v-if="list.length>0">
+        <li v-for="(item,index) in list" :key="index">
           <div class="flex">
             <div class="flex1">
               <p class="title">完善资料</p>
             </div>
-            <p class="change right text_r"><span class="changeNum add">+20</span></p>
+            <p class="change right text_r">
+              <span class="changeNum add">+20</span>
+            </p>
           </div>
           <div class="flex">
             <div class="flex1">
-              <p class="time">2018-08-12 11:06</p>
+              <p class="time">{{item.AddTime}}</p>
             </div>
-            <p class="num right text_r">积分：120</p>
+            <p class="num right text_r">积分：{{item.After}}</p>
           </div>
         </li>
         <li>
