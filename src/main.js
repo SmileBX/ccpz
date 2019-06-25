bc1b0053c55c1ab7effea46e9e0327894244f22d
Ans_xu <835858556@qq.com>
2019-06-25 09:48:53 +0800

    `

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ App.mpType = 'app'
 import store from './store/index'
 //把store挂载到全局
 Vue.prototype.$store = store;
+// 获取定位和城市编码
 Vue.prototype.getCityCode = getCityCode;
 const app = new Vue(App)
 app.$mount()
